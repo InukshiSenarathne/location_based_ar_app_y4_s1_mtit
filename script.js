a8646261b997b2e7434220285c354a1d44eed5b7
Inukshi Senarathne <senarathneinukshi@gmail.com>
2023-05-18 22:17:11 +0530

    restructure

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = () => {
-     let places = staticLoadPlaces();
-     renderPlaces(places);
+    let places = staticLoadPlaces();
+    renderPlaces(places);
 };
 
 function staticLoadPlaces() {
@@ -24,7 +24,7 @@ function renderPlaces(places) {
 
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+        model.setAttribute('gltf-model', './assets/main_object.png');
         model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
         model.setAttribute('scale', '0.5 0.5 0.5');
